70e23fab170864693cfa3c778d4282afeb0d8fec
rocheeli <bethsroche@gmail.com>
2016-07-22 16:16:43 -0400

    initial commit

---
+++ src/com/week1/day5/assignment1/MethodsAndFlow.java
@@ -0,0 +1,32 @@
+package com.week1.day5.assignment1;
+
+public class MethodsAndFlow {
+	private String[] stringArray;
+
+	public MethodsAndFlow(String[] stringArray)
+	{
+		this.stringArray = stringArray;
+
+	}
+
+	public void outputSubset(int startPosition, int numberOfValues)
+	{
+		if (startPosition >= 0 && startPosition < stringArray.length) 
+		{
+			for (int i = startPosition; i < (startPosition + numberOfValues); i++)
+
+			{
+				System.out.println(stringArray[i]);
+			}
+		} 
+		else 
+		{
+			System.out.println("Error");
+		}
+	}
+
+	public void outputAll() {
+		outputSubset(0, stringArray.length);
+
+	}
+}
---
+++ src/com/week1/day5/assignment1/TestMethodsAndFlow.java
@@ -0,0 +1,12 @@
+package com.week1.day5.assignment1;
+
+public class TestMethodsAndFlow {
+	
+	public static void main(String[] args)
+	{
+		MethodsAndFlow duke = new MethodsAndFlow(args);	
+		duke.outputAll();
+		duke.outputSubset(2,5 );
+	}
+	
+}
